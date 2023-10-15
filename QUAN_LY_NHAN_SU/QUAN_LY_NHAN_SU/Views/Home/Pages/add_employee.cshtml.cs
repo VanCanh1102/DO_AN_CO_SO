1144617f5a5e6797e51ec6fe4eb657c27e9370d2
VanCanh1102 <nvc11022003@gmail.com>
2023-10-15 14:26:57 +0700

    10/15/2023_Sua_Pages

---
+++ QUAN_LY_NHAN_SU/QUAN_LY_NHAN_SU/Views/Home/Pages/add_employee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QUAN_LY_NHAN_SU.Views.Home.Pages
+{
+    public class add_employeeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
